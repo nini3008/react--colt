914f8791c5b2668d08ec724361afb8621c51c39f
Nick iloba <nick.iloba@mullenlowe.com>
2019-06-04 16:57:26 -0400

    added a mew dice game

---
+++ basics/app1/dice-game/src/Die.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Die extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <p>Hey</p>
+      </div>
+    );
+  }
+}
+
+export default Die;
---
+++ basics/app1/dice-game/src/RollDice.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Die from "./Die";
+
+class Roll extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <Die />
+      </div>
+    );
+  }
+}
+
+export default Roll;
