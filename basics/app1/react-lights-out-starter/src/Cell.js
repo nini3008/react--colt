2c7728b0181343fa03d0348586c26701676912f2
Nick iloba <nick.iloba@mullenlowe.com>
2019-06-25 16:48:00 -0400

    working on the very confusing lightout game

--- basics/app1/react-lights-out-starter/src/Cell.js
+++ basics/app1/react-lights-out-starter/src/Cell.js
@@ -1,19 +1,18 @@
-import React, {Component} from 'react'
-import "./Cell.css"
+import React, { Component } from "react";
+import "./Cell.css";
 
-
-/** A single cell on the board.
- *
- * This has no state --- just two props:
- *
- * - flipCellsAroundMe: a function rec'd from the board which flips this
- *      cell and the cells around of it
- *
- * - isLit: boolean, is this cell lit?
- *
- * This handles clicks --- by calling flipCellsAroundMe
- *
- **/
+// /** A single cell on the board.
+//  *
+//  * This has no state --- just two props:
+//  *
+//  * - flipCellsAroundMe: a function rec'd from the board which flips this
+//  *      cell and the cells around of it
+//  *
+//  * - isLit: boolean, is this cell lit?
+//  *
+//  * This handles clicks --- by calling flipCellsAroundMe
+//  *
+//  **/
 
 class Cell extends Component {
   constructor(props) {
@@ -29,11 +28,8 @@ class Cell extends Component {
   render() {
     let classes = "Cell" + (this.props.isLit ? " Cell-lit" : "");
 
-    return (
-        <td className={classes} onClick={this.handleClick} />
-    )
+    return <td className={classes} onClick={this.handleClick} />;
   }
 }
 
-
-export default Cell
+export default Cell;
