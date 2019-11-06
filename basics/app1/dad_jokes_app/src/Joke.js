c1b75e472697b168138a1e65a4399139c570bf1b
Nicholas Iloba <nick_iloba@hotmail.com>
2019-11-05 19:40:21 -0500

    finshed the dad joke exercise

--- basics/app1/dad_jokes_app/src/Joke.js
+++ basics/app1/dad_jokes_app/src/Joke.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './Joke.css'
+import React, { Component } from "react";
+import "./Joke.css";
 
 class Joke extends Component {
     getColor() {
@@ -36,23 +36,22 @@ class Joke extends Component {
             return "em em-angry";
         }
     }
-
     render() {
         return (
-            <div className="Joke">
-                <div className="Joke-buttons">
-                    <i className="fas fa-arrow-up" onClick={this.props.upVote} />
-                    <span class="Joke-votes" style={{ borderColor: this.getColor() }}>{this.props.votes}</span>
-                    <i className="fas fa-arrow-down" onClick={this.props.downVote} />
+            <div className='Joke'>
+                <div className='Joke-buttons'>
+                    <i className='fas fa-arrow-up' onClick={this.props.upvote} />
+                    <span className='Joke-votes' style={{ borderColor: this.getColor() }}>
+                        {this.props.votes}
+                    </span>
+                    <i className='fas fa-arrow-down' onClick={this.props.downvote} />
                 </div>
-                <div className="Joke-text">{this.props.text}</div>
-                <div className="Joke-smiley">
-                    <i className={this.getEmoji()}></i>
+                <div className='Joke-text'>{this.props.text}</div>
+                <div className='Joke-smiley'>
+                    <i className={this.getEmoji()} />
                 </div>
-
             </div>
         );
     }
 }
-
 export default Joke;
