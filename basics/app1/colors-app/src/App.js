0c6c4d6aedf2114e6b9eef6e19ca62eb175adb3b
Nicholas Iloba <nick_iloba@hotmail.com>
2020-01-20 09:37:34 -0500

    fixed palette route

--- basics/app1/colors-app/src/App.js
+++ basics/app1/colors-app/src/App.js
@@ -1,17 +1,40 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColor";
 
 import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function(palette) {
+      return palette.id === id;
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      <Switch>
+        <Route exact path="/" render={() => <h1>testing route</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
     );
   }
 }
 
 export default App;
+
+{
+  /* <div className="App">
+<Palette palette={generatePalette(seedColors[4])} />
+</div> */
+}
