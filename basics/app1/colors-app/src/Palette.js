2bc3ac10fc80a0a3ce336127a2f72ca838263984
Nicholas Iloba <nick_iloba@hotmail.com>
2020-01-27 20:31:31 -0500

    back at it

--- basics/app1/colors-app/src/Palette.js
+++ basics/app1/colors-app/src/Palette.js
@@ -24,7 +24,12 @@ class Palette extends Component {
     const { level, format } = this.state;
 
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color[format]} name={color.name} key={color.id} />
+      <ColorBox
+        background={color[format]}
+        name={color.name}
+        key={color.id}
+        id={color.id}
+      />
     ));
     return (
       <div className="Palette">
---
+++ basics/app1/colors-app/src/SingleColorPalette.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class SingleColorPalette extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="">
+        <p>Single</p>
+      </div>
+    );
+  }
+}
+
+export default SingleColorPalette;
